227f22794fde5a95cdfd3cfc3cc2cd821d376194
Pete Smith <roysvork@gmail.com>
2013-08-18 18:55:13 +0100

    Initial Commit

---
+++ ObjectContentXmlBug/BugTest.cs
@@ -0,0 +1,59 @@
+﻿namespace ObjectContentXmlBug
+{
+    using System.Linq;
+    using System.Net;
+    using System.Web.Http;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using WebAPI.Testing;
+
+    [TestClass]
+    public class BugTest
+    {
+        private Browser browser;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var config = new HttpConfiguration();
+            config.Formatters.XmlFormatter.UseXmlSerializer = true;
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            browser = new Browser(config);
+        }
+
+        [TestMethod]
+        public void Fails()
+        {
+            var response = browser.Get(
+            "/api/datafail/",
+            (with) =>
+            {
+                with.Header("Accept", "application/xml");
+                with.HttpRequest();
+            });
+
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
+            Assert.IsTrue(response.Content.Headers.GetValues("Content-Type").FirstOrDefault().StartsWith("application/xml"));
+        }
+
+        [TestMethod]
+        public void Succeeds()
+        {
+            var response = browser.Get(
+            "/api/datasuccess/",
+            (with) =>
+            {
+                with.Header("Accept", "application/xml");
+                with.HttpRequest();
+            });
+
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
+            Assert.IsTrue(response.Content.Headers.GetValues("Content-Type").FirstOrDefault().StartsWith("application/xml"));
+        }
+    }
+}
---
+++ ObjectContentXmlBug/DataController.cs
@@ -0,0 +1,99 @@
+﻿namespace ObjectContentXmlBug
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Net.Http.Formatting;
+    using System.Web.Http;
+
+    [Serializable]
+    public class DataClass
+    {
+        private readonly string name;
+
+        private readonly int age;
+
+        private readonly bool awesome;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+        }
+
+        public bool Awesome
+        {
+            get
+            {
+                return awesome;
+            }
+        }
+
+        public DataClass(string name, int age, bool awesome)
+        {
+            this.name = name;
+            this.age = age;
+            this.awesome = awesome;
+        }
+    }
+
+    public class DataFailController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            var query =
+                new List<DataClass>
+                {
+                    new DataClass("Peter", 29, true),
+                    new DataClass("Kathryn", 26, false)
+                }.AsQueryable();
+
+            var conneg = (IContentNegotiator)GlobalConfiguration.Configuration.Services.GetService(typeof(IContentNegotiator));
+            var formatter = conneg.Negotiate(
+                query.GetType(),
+                Request,
+                GlobalConfiguration.Configuration.Formatters);
+
+            var response = Request.CreateResponse(HttpStatusCode.OK, query);
+            response.Content = new ObjectContent(query.GetType(), query, formatter.Formatter);
+
+            return response;
+        }
+    }
+
+    public class DataSuccessController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            var query =
+                new List<DataClass>
+                {
+                    new DataClass("Peter", 29, true),
+                    new DataClass("Kathryn", 26, false)
+                }.AsQueryable();
+
+            var conneg = (IContentNegotiator)GlobalConfiguration.Configuration.Services.GetService(typeof(IContentNegotiator));
+            var formatter = conneg.Negotiate(
+                query.GetType(),
+                Request,
+                GlobalConfiguration.Configuration.Formatters);
+
+            var response = Request.CreateResponse(HttpStatusCode.OK, query);
+            response.Content = new ObjectContent<IQueryable<DataClass>>(query, formatter.Formatter);
+
+            return response;
+        }
+    }
+}
